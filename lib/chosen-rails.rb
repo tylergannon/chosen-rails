743df1d29948913f9c4b97437b48d1db4921f2ed
Tyler Gannon <tgannon@gmail.com>
2016-04-04 19:30:11 +0800

    Make sure engine will always be loaded.

--- lib/chosen-rails.rb
+++ lib/chosen-rails.rb
@@ -6,12 +6,12 @@ module Chosen
 end
 
 case ::Rails.version.to_s
-when /^[4-5]/
-  require 'chosen-rails/engine'
 when /^3\.[12]/
   require 'chosen-rails/engine3'
 when /^3\.[0]/
   require 'chosen-rails/railtie'
+else
+  require 'chosen-rails/engine'
 end
 
 require 'compass-rails'
